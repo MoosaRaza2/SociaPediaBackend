d6193cec70d2e4866048d4af324acf95a47ac24b
MoosaRaza2 <rmoosa276@gmail.com>
2023-02-23 23:58:27 +0500

    feat: :sparkles: added env and mongodb connection string

--- index.js
+++ index.js
@@ -35,3 +35,17 @@ const storage = multer.diskStorage({
         cb(null, file.originalname);
     }
 })
+
+// mongoose connection
+
+const PORT = process.env.PORT || 6001;
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+
+}).then(() => {
+    app.listen(PORT, () => console.log(`server Port ${PORT}`))
+}).catch((err) => {
+    console.log(err)
+})
